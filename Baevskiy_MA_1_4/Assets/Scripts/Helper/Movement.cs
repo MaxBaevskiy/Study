e68f97be2f5612f05aeef3882cb4cc4a9b00cce0
MaxBaevskiy <139381899+MaxBaevskiy@users.noreply.github.com>
2023-07-16 02:50:06 +0300

    Square animation

--- Baevskiy_MA_1_4/Assets/Scripts/Helper/Movement.cs
+++ Baevskiy_MA_1_4/Assets/Scripts/Helper/Movement.cs
@@ -1,10 +1,108 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Helper
 {
     public class Movement
     {
+        public TypesOfMovement type { get; set; }
+        public float speed { get; set; }
+        public float step { get; set; }
+        public Transform transform { get; set; }
+        public Coroutine coroutine { get; set; }
+        public MonoBehaviour player { get; set; }
+
+        private Vector3 _startPosition = Vector3.zero;
+        private Animator _animator;
+        private string _animationStartPath = "EverydayMotionPackFree/Mecanim/female_chair";
+        private string _animationRunPath = "EverydayMotionPackFree/Mecanim/female_sprint";
+        private string _animationIdlePath = "EverydayMotionPackFree/Mecanim/female_pant";
+
+        public void StartMoving()
+        {
+            _animator = player.GetComponent<Animator>();
+
+            //new LookAt().Set(GameObject.Find("chair"), transform);
+
+            switch (type)
+            {
+                case TypesOfMovement.Square:
+                    player.StartCoroutine(StandUpAndRun());
+                    break;
+
+            }
+        }
+
+        private IEnumerator StandUpAndRun()
+        {
+            _animator.SetBool("StandUp", true);
+
+
+            while (_animator.IsInTransition(0) || !_animator.GetCurrentAnimatorStateInfo(0).IsName("female_idle_pant"))
+            {
+                yield return null;
+            }
+
+            if (_animator.GetCurrentAnimatorStateInfo(0).IsName("female_idle_pant"))
+            {
+                yield return Moving(step);
+
+                yield return Rotating(90);
+                yield return Moving(step / 2);
+
+                yield return Rotating(0);
+                yield return Moving(step);
+
+                yield return Rotating(-90);
+                yield return Moving(step);
+
+                yield return Rotating(-180);
+                yield return Moving(step);
+
+                yield return Rotating(-270);
+                yield return Moving(step / 2);
+
+                yield return Rotating(-180);
+                yield return Moving(step);
+
+
+                yield return Rotating(-180);
+
+            }
+        }
+
+        private IEnumerator Moving(float step)
+        {
+            _animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>(_animationRunPath);
+            Vector3 targetPosition = transform.position + transform.forward * step;
+
+            while (transform.position != targetPosition)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+                yield return null;
+            }
+
+            _animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>(_animationIdlePath);
+
+            yield break;
+        }
+
+        private IEnumerator Rotating(float angle)
+        {
+
+            while (transform.localEulerAngles.y < angle)
+            {
+                transform.localRotation *= Quaternion.Euler(0, 50f * speed * Time.deltaTime, 0);
+
+                yield return null;
+            }
+
+            transform.localRotation = Quaternion.Euler(0, angle, 0);
+
+            yield break;
+        }
+
+
     }
 }
--- Baevskiy_MA_1_4/Assets/Scripts/Player/PlayerController.cs
+++ Baevskiy_MA_1_4/Assets/Scripts/Player/PlayerController.cs
@@ -9,47 +9,21 @@ namespace Player
         [SerializeField, Range(0, 10)] private float _movementSpeed = 1;
         [SerializeField, Range(0, 10)] private float _stepSize = 5;
 
-        private Vector3 _startPosition = Vector3.zero;
-        private Animator _animator;
-        private Animation _animation;
-        private int _animationIDstart = 13170;
-        private int _animationIDrun = 14060;
-        private int _animationIDidle = 14058;
+        private Movement _userMovement;
 
         // Start is called before the first frame update
         void Start()
         {
-            _animator = GetComponent<Animator>();
-            _animator.SetBool("StandUp", true);
-
-            for (int i = 0; i < _animator.runtimeAnimatorController.animationClips.Length; i++)
+            _userMovement = new Movement
             {
-                AnimationClip clip = _animator.runtimeAnimatorController.animationClips[i];
+                step = _stepSize,
+                speed = _movementSpeed,
+                transform = transform,
+                type = _typeOfMovement,
+                player = this
+            };
 
-                Debug.Log(clip.name);
-            }
-
-        }
-
-        void Update()
-        {
-
-            /*
-            if (_animator.IsInTransition(0) && _animator.runtimeAnimatorController.GetInstanceID() == _animationIDstart)
-            {
-                _animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("EverydayMotionPackFree/Mecanim/female_sprint");
-            }
-
-            if (_animator.runtimeAnimatorController.GetInstanceID() == _animationIDrun)
-            {
-                _animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("EverydayMotionPackFree/Mecanim/female_pant");
-            }
-            if (_animator.runtimeAnimatorController.GetInstanceID() == _animationIDidle)
-            {
-                _animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>("EverydayMotionPackFree/Mecanim/female_chair");
-            }
-            */
-
+            _userMovement.StartMoving();
 
         }
     }
